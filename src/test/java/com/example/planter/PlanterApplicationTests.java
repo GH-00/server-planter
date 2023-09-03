5eed82fd38a2e13dbf2681a7d670f0118465c8f8
GH-00 <ghkim7319@gmail.com>
2023-09-03 21:27:07 +0900

    [setting]intial setting

---
+++ src/test/java/com/example/planter/PlanterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.planter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
